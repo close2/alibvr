d0ca3fa02921e33d83243674ce5c823beb984412
Christian Loitsch <christian@loitsch.com>
2016-04-26 22:46:56 +0200

    feat: remove examples and create an example project on github

--- examples/src/avr-gdb.sh
+++
@@ -1,2 +0,0 @@
-#avr-gdb --eval-command="target remote localhost:1234" --eval-command="file adapter.obj" --eval-command="load adapter.obj" --interpreter=mi2 -quiet
-avr-gdb -cd=/home/cl/projects/alibvr/examples/src/ $@
