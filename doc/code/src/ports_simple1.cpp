4bd77e393437db13d6553656e8da433792ebc8ec
Christian Loitsch <christian@loitsch.com>
2016-05-30 21:32:50 +0200

    doc: use snippet in README.md

---
+++ doc/code/src/ports_simple1.cpp
@@ -0,0 +1,19 @@
+#include "ports.h"
+
+__attribute__ ((OS_main)) int main(void) {
+  // «PORTS_DDR_PORT_PIN»
+  typedef PIN_C2 Pin_In;   // Give PIN_C2 a meaningful name.
+
+  // Use your favorite way of setting DDR of pin C2:
+  Pin_In::DDR = 0;         // PIN_C2::DDR = 0; would work as well.
+  Pin_In::DDR = ports::DataDirection::Read;  // Use the (safer) provided enum.
+  Pin_In::setDD(ports::DataDirection::Read); // Equivalent to the previous line.
+
+  Pin_In::setToInput(ports::PullUp::HighZ);  // Sets DDR and then PORT (pullup).
+
+  uint8_t i = Pin_In::PIN; // Read a value.
+  /*¤*/
+  
+  for (;;);
+  return i;
+}
--- mkdoc.sh
+++ mkdoc.sh
@@ -2,8 +2,13 @@
 
 MKDOC=$0
 P=$(dirname $MKDOC)
+DOC_CODE_DIR="doc/code"
 
 cd "$P"
+cd "$DOC_CODE_DIR"
+make clean
+
+cd -
 
 doxygen
 
