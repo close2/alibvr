8da87c595385c1582dad5c7ca50652f9fa0c09c4
Christian Loitsch <christian@loitsch.com>
2016-07-13 09:50:24 +0200

    doc

--- doxygen/html/search/all_0.js
+++ doxygen/html/search/all_0.js
@@ -7,6 +7,6 @@ var searchData=
   ['_5fpulse_5fuart',['_pulse_uart',['../d1/dd6/namespace__pulse__uart.html',1,'']]],
   ['_5fset_5for_5fget',['_set_or_get',['../d4/d49/namespaceports.html#aa339a9d178cb414c4cf243450d5887d2',1,'ports']]],
   ['_5fset_5for_5fget_5f8_5fbits',['_set_or_get_8_bits',['../d4/d49/namespaceports.html#ada41109199590d9ccafae0f6d2adaf99',1,'ports']]],
-  ['_5fset_5for_5fget_5f8_5fbits_5fregs',['_set_or_get_8_bits_regs',['../d4/d49/namespaceports.html#a49c5f4c094bbfe316f3d63897cd7f6a6',1,'ports']]],
+  ['_5fset_5for_5fget_5f8_5fbits_5fregs',['_set_or_get_8_bits_regs',['../d4/d49/namespaceports.html#a541ed8a2807f6d65c80cdf90cd7106eb',1,'ports']]],
   ['_5fset_5for_5fget_5ff',['_set_or_get_f',['../d4/d49/namespaceports.html#a75686ee2e9a291c0095dec6d73e4af8d',1,'ports']]]
 ];
