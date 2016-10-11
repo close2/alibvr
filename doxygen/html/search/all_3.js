487ec16252851465d84a1b7cae358a09d6d85f1f
Christian Loitsch <christian@loitsch.com>
2016-10-11 17:56:05 +0200

    doc: clock_reached improvements

--- doxygen/html/search/all_3.js
+++ doxygen/html/search/all_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['clearerrors',['ClearErrors',['../d7/d0d/class__transmission_1_1ClearErrors.html',1,'_transmission']]],
   ['clock',['clock',['../d9/d1f/namespaceclock.html',1,'']]],
-  ['clock_5freached',['clock_reached',['../d9/d1f/namespaceclock.html#a107ad02a77763be28bf63d43c566cf75',1,'clock::clock_reached(const T &amp;clock, const T &amp;previous_clock, const T &amp;target_clock)'],['../d9/d1f/namespaceclock.html#aae7a34ec30a2420b2b51e717ab02c400',1,'clock::clock_reached(const T &amp;previous_clock, const T &amp;target_clock)'],['../d9/d1f/namespaceclock.html#a240d81de9119f92701aa5f26555972c4',1,'clock::clock_reached(const T &amp;target_clock)']]],
+  ['clock_5freached',['clock_reached',['../d9/d1f/namespaceclock.html#a107ad02a77763be28bf63d43c566cf75',1,'clock::clock_reached(const T &amp;clock, const T &amp;previous_clock, const T &amp;target_clock)'],['../d9/d1f/namespaceclock.html#a240d81de9119f92701aa5f26555972c4',1,'clock::clock_reached(const T &amp;target_clock)']]],
   ['clockirqtask',['ClockIrqTask',['../d9/d1f/namespaceclock.html#aa67adb0b2215c44a4a770b6c36cfe8a7',1,'clock']]],
   ['clockselect',['ClockSelect',['../d9/d1f/namespaceclock.html#ac435d838e47eaebdb967aebfeab78eae',1,'clock']]],
   ['conditional',['conditional',['../d4/d6a/structstd_1_1conditional.html',1,'std']]],
